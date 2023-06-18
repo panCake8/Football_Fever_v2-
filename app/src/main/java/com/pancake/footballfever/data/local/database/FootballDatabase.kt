ab0d7049d4fc0bbdee548e2e2860b6fbdd4e8c3d
youssef <yr925344git@gmail.com>
2023-06-18 10:52:06 +0300

    update FootballDatabase

--- app/src/main/java/com/pancake/footballfever/data/local/database/FootballDatabase.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/database/FootballDatabase.kt
@@ -2,12 +2,21 @@ package com.pancake.footballfever.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.footboolfever.data.local.database.entity.CountryEntity
+import com.example.footboolfever.data.local.database.entity.FavoriteTeamEntity
+import com.example.footboolfever.data.local.database.entity.FixtureHomeEntity
+import com.example.footboolfever.data.local.database.entity.LeagueEntity
+import com.example.footboolfever.data.local.database.entity.VenueEntity
 import com.pancake.footballfever.data.local.database.daos.FootballDao
 
 
 @Database(
     entities = [
-
+        CountryEntity::class,
+        LeagueEntity::class,
+        VenueEntity::class,
+        FavoriteTeamEntity::class,
+        FixtureHomeEntity::class
     ],
     version = 1,
 )
