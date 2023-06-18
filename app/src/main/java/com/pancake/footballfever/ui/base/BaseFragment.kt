ff3488ba80b28c207eb1d2d67e53db399caa877b
youssef <yr925344git@gmail.com>
2023-06-18 13:15:55 +0300

    add base fragment

---
+++ app/src/main/java/com/pancake/footballfever/ui/base/BaseFragment.kt
@@ -0,0 +1,45 @@
+package com.pancake.footballfever.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.pancake.footballfever.BR
+
+abstract class BaseFragment<DB : ViewDataBinding, VM : ViewModel> : Fragment() {
+    private var _binding: DB? = null
+    protected val binding
+        get() = _binding!!
+
+    abstract val layoutId: Int
+    abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate<DB>(inflater, layoutId, container, false)
+            .apply {
+                lifecycleOwner = viewLifecycleOwner
+                setVariable(BR.viewModel, viewModel)
+            }
+        return _binding?.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    open fun initViews() {}
+}
