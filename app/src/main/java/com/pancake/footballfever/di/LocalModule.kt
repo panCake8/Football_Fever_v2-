7a6eb8cee6774779018254756ba906d8eb4a56c7
youssef <yr925344git@gmail.com>
2023-06-18 09:10:46 +0300

    add daos

---
+++ app/src/main/java/com/pancake/footballfever/di/LocalModule.kt
@@ -0,0 +1,38 @@
+package com.pancake.footballfever.di
+
+import android.content.Context
+import androidx.room.Room
+import com.pancake.footballfever.data.Constants
+import com.pancake.footballfever.data.local.database.FootballDatabase
+import com.pancake.footballfever.data.local.database.daos.FootballDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    @Singleton
+    @Provides
+    fun provideFootballDatabase(
+        @ApplicationContext context: Context,
+    ): FootballDatabase =
+        Room.databaseBuilder(
+            context,
+            FootballDatabase::class.java,
+            Constants.DATABASE_NAME,
+        ).build()
+
+    @Singleton
+    @Provides
+    fun provideFootBallDao(footballDataBase: FootballDatabase): FootballDao {
+        return footballDataBase.footballDao
+    }
+
+
+}
