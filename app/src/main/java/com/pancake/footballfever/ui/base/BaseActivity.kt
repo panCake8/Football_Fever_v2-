51054797c9b4ec4f68102b389a709081c5231e5d
youssef <yr925344git@gmail.com>
2023-06-18 13:09:04 +0300

    add base activity

---
+++ app/src/main/java/com/pancake/footballfever/ui/base/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.pancake.footballfever.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<DB: ViewDataBinding>: AppCompatActivity() {
+    abstract val layoutIdActivity: Int
+
+    private lateinit var _binding: DB
+    protected val binding get() = _binding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?,) {
+        super.onCreate(savedInstanceState)
+
+        _binding = DataBindingUtil.setContentView(this, layoutIdActivity)
+        binding.lifecycleOwner = this@BaseActivity
+
+    }
+}
