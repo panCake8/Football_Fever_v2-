77406d1b58591cc9aa9055792c948eeca7d11b79
youssef <yr925344git@gmail.com>
2023-06-18 13:15:36 +0300

    add base adapter

---
+++ app/src/main/java/com/pancake/footballfever/ui/base/BaseAdapter.kt
@@ -0,0 +1,51 @@
+package com.pancake.footballfever.ui.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.pancake.footballfever.BR
+
+
+interface BaseAdapterListener
+
+abstract class BaseAdapter<T>(
+    private val listener: BaseAdapterListener
+) : ListAdapter<T, BaseAdapter.BaseViewHolder>(BaseDiffUtil()) {
+
+    abstract val getLayoutId: Int
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return ItemViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                getLayoutId,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        when (holder) {
+            is ItemViewHolder -> bind(holder, position)
+        }
+    }
+
+    open fun bind(holder: ItemViewHolder, position: Int) {
+        holder.binding.apply {
+            val currentItem = getItem(position)
+            setVariable(BR.item, currentItem)
+            setVariable(BR.listener, listener)
+        }
+    }
+
+    open fun areTheSameItem(oldItem: T, newItem: T) = oldItem?.equals(newItem) == true
+
+    abstract class BaseViewHolder(binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+    class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
+
+
+}
