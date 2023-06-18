f47e37e51613387e2419dd6bc3fb5342d5772912
youssef <yr925344git@gmail.com>
2023-06-18 10:51:42 +0300

    update FootballDao

--- app/src/main/java/com/pancake/footballfever/data/local/database/daos/FootballDao.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/database/daos/FootballDao.kt
@@ -1,4 +1,52 @@
 package com.pancake.footballfever.data.local.database.daos
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.footboolfever.data.local.database.entity.CountryEntity
+import com.example.footboolfever.data.local.database.entity.FavoriteTeamEntity
+import com.example.footboolfever.data.local.database.entity.FixtureHomeEntity
+import com.example.footboolfever.data.local.database.entity.LeagueEntity
+import com.example.footboolfever.data.local.database.entity.VenueEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface FootballDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLeagues(leagues: List<LeagueEntity>)
+
+    @Query("SELECT * FROM LEAGUE_TABLE ORDER BY id DESC")
+    fun getAllLeagues(): List<LeagueEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVenues(venues: List<VenueEntity>)
+
+    @Query("SELECT * FROM VENUE_TABLE ORDER BY id DESC")
+    fun getAllVenues(): List<VenueEntity>
+
+    @Query("SELECT * FROM VENUE_TABLE WHERE id = :venueId")
+    fun getVenueById(venueId: Int): VenueEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCountries(countries: List<CountryEntity>)
+
+    @Query("SELECT * FROM COUNTRY_TABLE ORDER BY id DESC")
+    fun getAllCountries(): List<CountryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavoriteTeam(team: List<FavoriteTeamEntity>)
+
+    @Query("select * from FAVORITE_TEAM_TABLE")
+    suspend fun getAllFavoriteTeams(): List<FavoriteTeamEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFixtureHome(teams: List<FixtureHomeEntity>)
+
+    @Query("select * from FIXTURE_TEAM_ENTITY")
+    fun getAllFixtureHome(): Flow<List<FixtureHomeEntity>>
+
+    @Query("delete from FIXTURE_TEAM_ENTITY ")
+    fun deleteAllFixtureHome()
 }
