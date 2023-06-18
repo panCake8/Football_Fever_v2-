e83281c607f984a6496a4b4d5d19254949a0b845
youssef <yr925344git@gmail.com>
2023-06-18 08:25:54 +0300

    add RemoteModule

---
+++ app/src/main/java/com/pancake/footballfever/di/RemoteModule.kt
@@ -0,0 +1,64 @@
+package com.pancake.footballfever.di
+
+import com.pancake.footballfever.BuildConfig
+import com.pancake.footballfever.data.remote.service.ApiService
+import com.pancake.footballfever.data.remote.Interceptor.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+
+    @Singleton
+    @Provides
+    fun provideFootballApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClint(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverter(): GsonConverterFactory = GsonConverterFactory.create()
+
+
+}
