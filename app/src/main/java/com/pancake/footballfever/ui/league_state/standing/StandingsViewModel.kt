830e1be8c4fb9f50ce1041d8ceed6e6cc9f1d4a6
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 18:41:40 +0300

    update StandingsViewModel

--- app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
@@ -1,12 +1,19 @@
 package com.pancake.footballfever.ui.league_state.standing
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.pancake.footballfever.domain.usecase.FetchStandingsAndCacheUseCase
 import com.pancake.footballfever.domain.usecase.GetCachedStandingsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
@@ -19,11 +26,36 @@ class StandingsViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(StandingsUIState())
     val uiState: StateFlow<StandingsUIState> = _uiState
 
+
+
     init {
-        Log.i("test","test--------------------")
-        fetchStandingsAndCacheUseCase
+        _uiState.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                fetchStandingsAndCacheUseCase.invoke(39, 2019)
 
+                _uiState.update { it.copy(response = getCachedStandingsUseCase.invoke(39, 2019), isLoading = false) }
+                Log.i("test","${_uiState.value}")
 
+            }
+        }
     }
+
+
+
+
+//    private suspend fun showStandingsLeague(league: Int, season: Int) {
+//        viewModelScope.launch {
+//            _uiState.update { it.copy(isLoading = true) }
+//            _uiState.update {
+//                it.copy(response = getStandingsUseCase.invoke(league, season), isLoading = false)
+//            }
+//        }
+//    }
+
+
+
+
+
 
 }
