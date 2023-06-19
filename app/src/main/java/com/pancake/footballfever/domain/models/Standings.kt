fd5a855607ee204db11763b763ef229b990bfb65
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 18:40:52 +0300

    update data standings

--- app/src/main/java/com/pancake/footballfever/domain/models/Standings.kt
+++ app/src/main/java/com/pancake/footballfever/domain/models/Standings.kt
@@ -1,11 +1,14 @@
 package com.pancake.footballfever.domain.models
 
 import com.example.footboolfever.data.remote.dto.standings.Team
+import com.google.gson.annotations.SerializedName
 
 
 data class Standings(
     val rank:Int? =null,
-    val team: Team? = null,
+    val teamName: String? = null,
+    val teamLogoUrl: String? = null,
+    val teamId: Int? = null,
     val points: Int? = null,
     val win: Int? = null,
     val played:Int? = null
