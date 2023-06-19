697f7d83f562a2c5825840776dc0d0426eab5077
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 16:27:11 +0300

    update StandingsViewModel

--- app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
@@ -1,39 +1,29 @@
 package com.pancake.footballfever.ui.league_state.standing
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.pancake.footballfever.ui.league_state.standing.StandingsListener
-import com.pancake.footballfever.ui.league_state.standing.StandingsUIState
+import com.pancake.footballfever.domain.usecase.FetchStandingsAndCacheUseCase
+import com.pancake.footballfever.domain.usecase.GetCachedStandingsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class StandingsViewModel @Inject constructor(
-    private val getStandingsUseCase: GetStandingsUseCase
+    private val fetchStandingsAndCacheUseCase: FetchStandingsAndCacheUseCase,
+    private val getCachedStandingsUseCase: GetCachedStandingsUseCase
 
 ) : ViewModel(), StandingsListener {
     private val _uiState = MutableStateFlow(StandingsUIState())
     val uiState: StateFlow<StandingsUIState> = _uiState
 
     init {
-        viewModelScope.launch {
-            showStandingsLeague(39, 2019)
+        Log.i("test","test--------------------")
+        fetchStandingsAndCacheUseCase
 
-        }
 
     }
 
-    private suspend fun showStandingsLeague(league: Int, season: Int) {
-        viewModelScope.launch {
-            _uiState.update { it.copy(isLoading = true) }
-            _uiState.update {
-                it.copy(response = getStandingsUseCase.invoke(league, season), isLoading = false)
-            }
-        }
-    }
 }
