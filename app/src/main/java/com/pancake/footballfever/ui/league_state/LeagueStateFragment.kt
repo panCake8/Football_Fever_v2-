fe22867adb52fc69c9afe9a76eafd10f418e42d9
youssef <yr925344git@gmail.com>
2023-06-19 14:27:00 +0300

    update LeagueStateFragment

--- app/src/main/java/com/pancake/footballfever/ui/league_state/LeagueStateFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/LeagueStateFragment.kt
@@ -3,9 +3,12 @@ package com.pancake.footballfever.ui.league_state
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentLeagueStateBinding
 import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.league_state.match.MatchFragment
+import com.pancake.footballfever.ui.league_state.standing.StandingFragment
 import com.pancake.footballfever.ui.league_state.top_assist.TopAssistFragment
 import com.pancake.footballfever.ui.league_state.top_score.TopScoreFragment
 
@@ -21,7 +24,8 @@ class LeagueStateFragment : BaseFragment<FragmentLeagueStateBinding, LeagueState
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        initViewPager()
+        initTabLayout()
     }
 
     private fun initViewPager() {
@@ -32,9 +36,17 @@ class LeagueStateFragment : BaseFragment<FragmentLeagueStateBinding, LeagueState
 
     private fun addFragmentToViewPager() {
 
+        leagueStatePagerAdapter.addFragment(MatchFragment())
+        leagueStatePagerAdapter.addFragment(StandingFragment())
         leagueStatePagerAdapter.addFragment(TopScoreFragment())
         leagueStatePagerAdapter.addFragment(TopAssistFragment())
 
+    }
+
+    private fun initTabLayout() {
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            tab.text = tabItems[position]
+        }.attach()
     }
 
     companion object {
