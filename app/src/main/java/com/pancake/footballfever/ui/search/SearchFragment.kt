65efc94378da365ae79151c3e201d7b15fcc4232
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-19 18:50:48 +0300

    update SearchFragment

--- app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
@@ -2,21 +2,22 @@ package com.pancake.footballfever.ui.search
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentSearchBinding
 import com.pancake.footballfever.ui.base.BaseFragment
 
 
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
-    override val layoutId: Int
-        get() = TODO("Not yet implemented")
-    override val viewModel: SearchViewModel
-        get() = TODO("Not yet implemented")
+    override val layoutId: Int= R.layout.fragment_search
+    override val viewModel: SearchViewModel by viewModels()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.searchRecycler.adapter=SearchAdapter(viewModel)
+        binding.viewModel=viewModel
     }
 
 
