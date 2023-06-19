a769cbd01761b3c39f48ceafb0d0328eec591c74
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 16:25:29 +0300

    update StandingsRepository

--- app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
@@ -3,7 +3,7 @@ package com.pancake.footballfever.data.repository
 import com.pancake.footballfever.data.local.database.entity.StandingsEntity
 
 interface StandingsRepository {
-    suspend fun getAllStandings(league:Int,season:Int): List<StandingsEntity>
+    suspend fun getCachedStandings(leagueId:Int,season:Int): List<StandingsEntity>
 
-    suspend fun fetchStandingsAndCache(league:Int,season:Int)
+    suspend fun fetchStandingsAndCache(leagueId:Int,season:Int)
 }
