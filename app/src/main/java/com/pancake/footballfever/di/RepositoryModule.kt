be2007ffd9c19c8c80efa517343f08e4286b9b59
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 16:24:35 +0300

    add bindsStandingsRepository

--- app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
@@ -1,12 +1,20 @@
 package com.pancake.footballfever.di
 
+import com.pancake.footballfever.data.repository.StandingsRepository
+import com.pancake.footballfever.data.repository.StandingsRepositoryImp
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-
+    @Singleton
+    @Binds
+    abstract fun bindsStandingsRepository(
+        standingsRepositoryImp: StandingsRepositoryImp
+    ): StandingsRepository
 
 }
