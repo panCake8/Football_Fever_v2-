1b8c710b20a336d7b0d160db632ea1c6ee3d4163
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 14:51:43 +0300

    update repository

--- app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
@@ -1,7 +1,6 @@
 package com.pancake.footballfever.data.repository
 
 import com.pancake.footballfever.data.local.database.entity.StandingsEntity
-import kotlinx.coroutines.flow.Flow
 
 interface StandingsRepository {
     suspend fun getAllStandings(league:Int,season:Int): List<StandingsEntity>
--- app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepositoryImp.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepositoryImp.kt
@@ -2,13 +2,16 @@ package com.pancake.footballfever.data.repository
 
 import com.pancake.footballfever.data.local.database.daos.FootballDao
 import com.pancake.footballfever.data.local.database.entity.StandingsEntity
+import com.pancake.footballfever.data.local.mappers.StandingsMapper
 import com.pancake.footballfever.data.remote.service.ApiService
 import javax.inject.Inject
 
 class StandingsRepositoryImp @Inject constructor(
     private val apiService: ApiService,
     private val dao: FootballDao,
+    private val mapper:StandingsMapper
 ) : StandingsRepository {
+
 
     override suspend fun getAllStandings(league: Int, season: Int): List<StandingsEntity> {
         return dao.getAllStandings(league, season)
@@ -16,28 +19,8 @@ class StandingsRepositoryImp @Inject constructor(
 
     override suspend fun fetchStandingsAndCache(league: Int, season: Int) {
         val response = apiService.getStandingsLeague(league, season)
-        val items = response.body()
-            ?.response
-            ?.get(FIRST_ITEM)
+        val items = response.body()?.response?.let { mapper.map(it.get(0)) }
+        items?.let { dao.insertStandings(items) }
     }
-
-//    override suspend fun getStandingsLeagueLocal(league: Int, season: Int): List<StandingsEntity> {
-//        if (dao.getAllStandings().isEmpty()) {
-//            val response = getStandingsLeagueRemote(league, season)[FIRST_ITEM]
-//                .league
-//                ?.standings
-//                ?.get(FIRST_ITEM)!!
-//                .map(standingsMapper::map)
-//
-//            dao.insertStandings(response)
-//
-//        }
-//        return dao.getAllStandings()
-//    }
-
-    companion object {
-        const val FIRST_ITEM = 0
-    }
-
 
 }
