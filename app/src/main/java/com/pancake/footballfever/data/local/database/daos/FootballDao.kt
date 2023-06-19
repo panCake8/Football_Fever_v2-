f72aaf51b1ddc5751d9e2b8672c5a7ce7e22fde9
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 01:29:25 +0300

    add coaches dao

--- app/src/main/java/com/pancake/footballfever/data/local/database/daos/FootballDao.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/database/daos/FootballDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.pancake.footballfever.data.local.database.entity.CoachEntity
 import com.pancake.footballfever.data.local.database.entity.CountryEntity
 import com.pancake.footballfever.data.local.database.entity.FavoriteTeamEntity
 import com.pancake.footballfever.data.local.database.entity.FixtureHomeEntity
@@ -42,4 +43,10 @@ interface FootballDao {
 
     @Query("delete from FIXTURE_TEAM_TABLE ")
     fun deleteAllFixtureHome()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCoaches(coaches: List<CoachEntity>)
+
+    @Query("SELECT * FROM COACHES_TABLE ORDER BY id DESC")
+    fun getAllCoaches(): List<CoachEntity>
 }
