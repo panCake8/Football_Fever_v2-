1cf8383cd4b04b3cf2c52c2f8b6433c8e6524ef1
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2023-06-19 21:36:53 +0300

    add club tables fragment to package

--- app/src/main/java/com/pancake/footballfever/ui/club/tables/ClubTablesFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/club/tables/ClubTablesFragment.kt
@@ -1,10 +1,11 @@
-package com.pancake.footballfever
+package com.pancake.footballfever.ui.club.tables
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pancake.footballfever.R
 
 class ClubTablesFragment : Fragment() {
 
