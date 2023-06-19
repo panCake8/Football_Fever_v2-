3969310c5c74dc80760168f20ac02b551d676c14
youssef <yr925344git@gmail.com>
2023-06-19 18:37:07 +0300

    update RepositoryModule

--- app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
@@ -1,12 +1,20 @@
 package com.pancake.footballfever.di
 
+import com.pancake.footballfever.data.local.database.repository.PlayerRepository
+import com.pancake.footballfever.data.local.database.repository.PlayerRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
-
+    @Singleton
+    @Binds
+    abstract fun bindsPlayerRepository(
+        playerRepositoryImpl: PlayerRepositoryImpl
+    ) : PlayerRepository
 }
