bf3a06dd97d571ffada20de3af2c192a3da6a692
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-19 11:38:43 +0300

    add import

--- app/src/main/java/com/pancake/footballfever/ui/standings/StandingsViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/standings/StandingsViewModel.kt
@@ -1,8 +1,13 @@
 package com.pancake.footballfever.ui.standings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 @HiltViewModel
