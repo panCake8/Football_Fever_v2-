3a1ab65b349afd1aeb3f7f48316671c4bc648675
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-06-20 23:58:36 +0300

    update fixture fragment

--- app/src/main/java/com/pancake/footballfever/domain/workManager/FetchFixtureWorker.kt
+++ app/src/main/java/com/pancake/footballfever/domain/workManager/FetchFixtureWorker.kt
@@ -1,24 +1,20 @@
 package com.pancake.footballfever.domain.workManager
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.pancake.footballfever.domain.Constants
 import com.pancake.footballfever.domain.usecases.fixutreUseCases.FetchAndCacheFixtureUseCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 class FetchFixtureWorker @Inject constructor(
     context: Context,
     params: WorkerParameters,
     private val fetchAndCacheFixtureUseCase: FetchAndCacheFixtureUseCase
-) : Worker(context, params) {
-    override fun doWork(): Result {
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
         val fixtureId = inputData.getInt(Constants.KEY_FIXTURE_ID, 0)
-        val fetchDataResult = runBlocking(Dispatchers.IO) {
-            fetchAndCacheFixtureUseCase(fixtureId)
-        }
+        val fetchDataResult = fetchAndCacheFixtureUseCase(fixtureId)
         return if (fetchDataResult.isSuccess) {
             Result.success()
         } else {
--- app/src/main/java/com/pancake/footballfever/ui/fixture/FixtureFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/fixture/FixtureFragment.kt
@@ -2,13 +2,22 @@ package com.pancake.footballfever.ui.fixture
 
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.google.android.material.tabs.TabLayoutMediator
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentFixtureBinding
+import com.pancake.footballfever.domain.Constants
+import com.pancake.footballfever.domain.workManager.FetchFixtureWorker
 import com.pancake.footballfever.ui.base.BaseFragment
 import com.pancake.footballfever.ui.fixture.lineup.FragmentFixtureLineup
 import com.pancake.footballfever.ui.fixture.stats.FragmentFixtureStats
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class FixtureFragment : BaseFragment<FragmentFixtureBinding, FixtureViewModel>() {
@@ -29,6 +38,7 @@ class FixtureFragment : BaseFragment<FragmentFixtureBinding, FixtureViewModel>()
         fixtureId = arguments.fixtureId
         initViewPager()
         initTabLayout()
+        initWorkManager(fixtureId)
     }
 
     private fun initViewPager() {
@@ -52,11 +62,38 @@ class FixtureFragment : BaseFragment<FragmentFixtureBinding, FixtureViewModel>()
     }
 
 
+    private fun initWorkManager(fixtureId: Int?) {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val inputData = fixtureId?.let {
+            Data.Builder()
+                .putInt(Constants.KEY_FIXTURE_ID, it)
+                .build()
+        }
+        val request = inputData?.let {
+            PeriodicWorkRequestBuilder<FetchFixtureWorker>(16, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .setInputData(it)
+                .build()
+        }
+        request?.let {
+            WorkManager.getInstance(requireContext()).enqueueUniquePeriodicWork(
+                FIXTURE_WORK_MANAGER,
+                ExistingPeriodicWorkPolicy.KEEP,
+                it
+            )
+        }
+
+    }
+
+
     companion object {
         private const val STATS = "Stats"
         private const val SUMMARY = "Summary"
         private const val LINEUP = "Lineup"
         private const val TABLE = "Table"
         private const val H2H = "H2H"
+        private const val FIXTURE_WORK_MANAGER = "fixtureWorkManager"
     }
 }
