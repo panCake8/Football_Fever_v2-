5e248e2f28b71313ca588ada00af8212a114c38b
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-20 20:37:43 +0300

    update  BindingAdapter

--- app/src/main/java/com/pancake/footballfever/utilities/BindingAdapter.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/BindingAdapter.kt
@@ -61,3 +61,37 @@ fun ImageView.bindFixtureEventImg(type: String) {
         }
     )
 }
+
+@BindingAdapter("app:showWhenSearchLoading")
+fun <T> showWhenLoading(view: View,dataState: DataState<T>){
+
+    if(dataState is DataState.Loading){
+        view.visibility=View.VISIBLE
+    }
+    else
+        view.visibility=View.GONE
+
+}
+
+@BindingAdapter("app:showWhenSearchSuccess")
+fun <T> showWhenSuccess(view: View,dataState: DataState<T>){
+
+    if(dataState is DataState.Success){
+        view.visibility=View.VISIBLE
+    }
+    else
+        view.visibility=View.GONE
+
+}
+
+
+@BindingAdapter("app:showWhenSearchError")
+fun <T> showWhenError(view: View,dataState: DataState<T>){
+
+    if(dataState is DataState.Error){
+        view.visibility=View.VISIBLE
+    }
+    else
+        view.visibility=View.GONE
+
+}
