84eca406d277cf91f4afa8ca337ee408c354a11e
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 17:08:59 +0300

    add coach adapter

---
+++ app/src/main/java/com/pancake/footballfever/ui/coach/CoachAdapter.kt
@@ -0,0 +1,12 @@
+package com.pancake.footballfever.ui.coach
+
+import com.pancake.footballfever.R
+import com.pancake.footballfever.domain.models.Coaches
+import com.pancake.footballfever.ui.base.BaseAdapter
+import com.pancake.footballfever.ui.base.BaseAdapterListener
+
+class CoachAdapter(listener: BaseAdapterListener) : BaseAdapter<Coaches>(listener) {
+
+    override val getLayoutId: Int
+        get() = R.layout.item_coaches
+}
