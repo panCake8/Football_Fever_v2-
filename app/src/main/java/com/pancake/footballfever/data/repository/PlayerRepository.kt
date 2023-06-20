c9ba1e2706ea833d3c7f97ea213d6fe9abb423c6
youssef <yr925344git@gmail.com>
2023-06-20 17:01:57 +0300

    update player repository

--- app/src/main/java/com/pancake/footballfever/data/repository/PlayerRepository.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/PlayerRepository.kt
@@ -1,4 +1,17 @@
 package com.pancake.footballfever.data.repository
 
+import com.example.footboolfever.data.remote.dto.TopScoresDto
+import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
+
 interface PlayerRepository {
+
+    // get all scorer data.
+    suspend fun fetchTopGoalsDataAndCache(league: Int, season: Int): Result<List<TopGoalsEntity>>
+    suspend fun getTopGoalsCachedData(): List<TopGoalsEntity>
+
+    suspend fun getAllRemoteTopScorer( league: Int, season: Int,): List<TopScoresDto>?
+
+    suspend fun saveData(data: List<TopGoalsEntity>)
+
+
 }
--- app/src/main/java/com/pancake/footballfever/data/repository/PlayerRepositoryImpl.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/PlayerRepositoryImpl.kt
@@ -1,6 +1,47 @@
 package com.pancake.footballfever.data.repository
 
+import com.example.footboolfever.data.remote.dto.TopScoresDto
+import com.pancake.footballfever.data.local.database.daos.PlayerDao
+import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
+import com.pancake.footballfever.data.local.mappers.playerMapper.TopScorerMapper
+import com.pancake.footballfever.data.remote.service.ApiService
+
 import javax.inject.Inject
 
-class PlayerRepositoryImpl @Inject constructor() : PlayerRepository {
+class PlayerRepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val playerDao: PlayerDao,
+    private val topScorerMapper: TopScorerMapper,
+): PlayerRepository {
+
+    override suspend fun fetchTopGoalsDataAndCache(league: Int, season: Int): Result<List<TopGoalsEntity>> {
+        try {
+
+            val response = apiService.getTopScorerPlayers(league, season)
+            if (response.isSuccessful) {
+                val data = response.body()?.response
+                data?.let {
+                    playerDao.insertTopScorer(it.map(topScorerMapper::map))
+                    return Result.success(it.map(topScorerMapper::map))
+                }
+            }
+            return Result.failure(Exception("Failed to retrieve data"))
+
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+
+    override suspend fun getTopGoalsCachedData(): List<TopGoalsEntity> {
+        return playerDao.getAllTopScorers()
+    }
+
+    override suspend fun getAllRemoteTopScorer(league: Int, season: Int): List<TopScoresDto>? =
+        apiService.getTopScorerPlayers(league, season).body()?.response
+
+    override suspend fun saveData(data: List<TopGoalsEntity>) {
+        playerDao.insertTopScorer(data)
+    }
+
+
 }
