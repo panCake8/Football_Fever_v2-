6d2ce0096a9c8943600088a9d7a417aec6d6748e
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 14:35:52 +0300

    add coach fragment

---
+++ app/src/main/java/com/pancake/footballfever/ui/coach/CoachFragment.kt
@@ -0,0 +1,13 @@
+package com.pancake.footballfever.ui.coach
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CoachFragment : Fragment() {
+
+
+}
