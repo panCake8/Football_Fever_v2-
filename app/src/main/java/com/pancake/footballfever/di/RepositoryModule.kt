27e160340857b9a4b5452c0b19ed0e3fdc0aacde
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 14:34:43 +0300

    add dependency injection of coach repo

--- app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
@@ -1,12 +1,23 @@
 package com.pancake.footballfever.di
 
+import com.pancake.footballfever.data.repository.IListCoachRepository
+import com.pancake.footballfever.data.repository.ListCoachRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
+    @Binds
+    @Singleton
+    abstract fun listCoachRepository(
+        listRepository: ListCoachRepository
+    ): IListCoachRepository
+
 
 }
+
