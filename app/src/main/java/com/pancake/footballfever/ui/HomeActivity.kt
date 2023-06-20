aae2fbc9f9e42130693e6cac85a52c2b4e57da84
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-06-20 19:56:07 +0300

    add SharedPrefManager & update HomeActivity

--- app/src/main/java/com/pancake/footballfever/ui/HomeActivity.kt
+++ app/src/main/java/com/pancake/footballfever/ui/HomeActivity.kt
@@ -1,12 +1,19 @@
 package com.pancake.footballfever.ui
 
+import android.os.Bundle
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.ActivityHomeBinding
 import com.pancake.footballfever.ui.base.BaseActivity
+import com.pancake.footballfever.utilities.SharedPrefManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity<ActivityHomeBinding>() {
     override val layoutId: Int
         get() = R.layout.activity_home
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SharedPrefManager.getInit(applicationContext)
+    }
 }
