afe200ecf16086cf10df8b08bea8b116b3d8d51c
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 21:48:44 +0300

    add coach listener to adapter

--- app/src/main/java/com/pancake/footballfever/ui/coach/CoachAdapter.kt
+++ app/src/main/java/com/pancake/footballfever/ui/coach/CoachAdapter.kt
@@ -5,7 +5,7 @@ import com.pancake.footballfever.domain.models.Coaches
 import com.pancake.footballfever.ui.base.BaseAdapter
 import com.pancake.footballfever.ui.base.BaseAdapterListener
 
-class CoachAdapter(listener: BaseAdapterListener) : BaseAdapter<Coaches>(listener) {
+class CoachAdapter(listener: CoachesListener) : BaseAdapter<Coaches>(listener) {
 
     override val getLayoutId: Int
         get() = R.layout.item_coaches
