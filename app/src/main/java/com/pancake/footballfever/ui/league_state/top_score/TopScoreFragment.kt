3fb74c95a7380193b55a110664b2f86394951cd6
youssef <yr925344git@gmail.com>
2023-06-20 17:03:13 +0300

    update TopScoreFragment

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScoreFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScoreFragment.kt
@@ -1,7 +1,15 @@
 package com.pancake.footballfever.ui.league_state.top_score
 
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.pancake.footballfever.R
+import com.pancake.footballfever.databinding.FragmentTopScoreBinding
+import com.pancake.footballfever.ui.base.BaseFragment
 
-class TopScoreFragment : Fragment() {
+class TopScoreFragment : BaseFragment<FragmentTopScoreBinding, TopScorerViewModel>() {
+
+    override val layoutId: Int = R.layout.fragment_top_score
+    override val viewModel: TopScorerViewModel by viewModels()
+
+
 
 }
