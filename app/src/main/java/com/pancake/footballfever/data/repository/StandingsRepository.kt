a0aae66cdad8b3ec4bf0f893b0886c449fd5a267
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-20 22:19:34 +0300

    use Result<List<StandingsEntity>> as output type

--- app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepository.kt
@@ -3,7 +3,7 @@ package com.pancake.footballfever.data.repository
 import com.pancake.footballfever.data.local.database.entity.StandingsEntity
 
 interface StandingsRepository {
-    suspend fun getCachedStandings(leagueId:Int,season:Int): List<StandingsEntity>
+    suspend fun getCachedStandings(leagueId: Int, season: Int): List<StandingsEntity>
 
-    suspend fun fetchStandingsAndCache(leagueId:Int,season:Int)
+    suspend fun fetchStandingsAndCache(leagueId: Int, season: Int): Result<List<StandingsEntity>>
 }
--- app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepositoryImp.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/StandingsRepositoryImp.kt
@@ -9,18 +9,36 @@ import javax.inject.Inject
 class StandingsRepositoryImp @Inject constructor(
     private val apiService: ApiService,
     private val dao: FootballDao,
-    private val mapper:StandingsMapper
+    private val mapper: StandingsMapper
 ) : StandingsRepository {
 
 
-    override suspend fun getCachedStandings(leagueId:Int,season:Int): List<StandingsEntity> {
+    override suspend fun getCachedStandings(leagueId: Int, season: Int): List<StandingsEntity> {
         return dao.getAllStandings(leagueId, season)
     }
 
-    override suspend fun fetchStandingsAndCache(leagueId:Int,season:Int){
-        val response = apiService.getStandingsLeague(leagueId, season)
-        val items = response.body()?.response?.let { mapper.map(it.get(0)) }
-        items?.let { dao.insertStandings(items) }
+    override suspend fun fetchStandingsAndCache(
+        leagueId: Int,
+        season: Int
+    ): Result<List<StandingsEntity>> {
+        try {
+            val response = apiService.getStandingsLeague(leagueId, season)
+            if (response.isSuccessful) {
+
+                val items = response.body()?.response?.let { mapper.map(it.get(0)) }
+                items?.let {
+                    dao.insertStandings(items)
+                    return Result.success(items)
+                }
+
+            }
+            return Result.failure(Throwable( response.message().toString()))
+        } catch (e: Exception) {
+            return Result.failure(e)
+
+        }
+
+
     }
 
 }
