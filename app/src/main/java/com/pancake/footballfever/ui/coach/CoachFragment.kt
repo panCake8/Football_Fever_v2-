95279fe13a7380cdced04292e036d64363c8e8aa
Bassam <bassamouf@std.mans.edu.eg>
2023-06-21 01:27:40 +0300

    add coach fragment logic

--- app/src/main/java/com/pancake/footballfever/ui/coach/CoachFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/coach/CoachFragment.kt
@@ -1,14 +1,21 @@
 package com.pancake.footballfever.ui.coach
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentCoachBinding
+import com.pancake.footballfever.domain.workmanager.FetchCoachesWorker
 import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.coach.adapter.CoachAdapter
+import com.pancake.footballfever.ui.selectCountry.adapter.SelectCountryAdapter
+import java.util.concurrent.TimeUnit
 
 
 class CoachFragment : BaseFragment<FragmentCoachBinding,CoachViewModel>() {
@@ -17,6 +24,34 @@ class CoachFragment : BaseFragment<FragmentCoachBinding,CoachViewModel>() {
 
     override val viewModel: CoachViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        schedule(requireContext())
+    }
+
+
+    private fun setupRecyclerView() {
+        binding.coachesRecycler.adapter = CoachAdapter(viewModel)
+    }
+
+    companion object {
+        fun schedule(context: Context) {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val request = PeriodicWorkRequestBuilder<FetchCoachesWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+                "fetchCoachesWorker",
+                ExistingPeriodicWorkPolicy.KEEP,
+                request,
+            )
+        }
+    }
 
 
 }
