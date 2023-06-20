09c6c21ed2760e4c0f4dc6666f5d20edc8147025
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-20 21:02:34 +0300

    add fetchDataPeriodically

--- app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/standing/StandingsViewModel.kt
@@ -1,10 +1,18 @@
 package com.pancake.footballfever.ui.league_state.standing
 
+import FetchStandingsWorker
+import android.content.Context
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import androidx.work.workDataOf
 import com.pancake.footballfever.domain.usecase.FetchStandingsAndCacheUseCase
 import com.pancake.footballfever.domain.usecase.GetCachedStandingsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,21 +22,26 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 
 @HiltViewModel
 class StandingsViewModel @Inject constructor(
     private val fetchStandingsAndCacheUseCase: FetchStandingsAndCacheUseCase,
-    private val getCachedStandingsUseCase: GetCachedStandingsUseCase
+    private val getCachedStandingsUseCase: GetCachedStandingsUseCase,
+//    private val context: Context
 
 ) : ViewModel(), StandingsListener {
     private val _uiState = MutableStateFlow(StandingsUIState())
     val uiState: StateFlow<StandingsUIState> = _uiState
 
-
+//    private val workManager = WorkManager.getInstance(context)
 
     init {
+
+//        fetchDataPeriodically()
+
 
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
@@ -41,6 +54,34 @@ class StandingsViewModel @Inject constructor(
         }
     }
 
+//    private fun fetchDataPeriodically() {
+//        val constraints = Constraints.Builder()
+//            .setRequiredNetworkType(NetworkType.CONNECTED)
+//            .build()
+//
+//        val request = PeriodicWorkRequestBuilder<FetchStandingsWorker>(1, TimeUnit.HOURS)
+//            .setConstraints(constraints)
+//            .build()
+//
+//        workManager.enqueueUniquePeriodicWork(
+//            FetchStandingsWorker.WORK_NAME,
+//            ExistingPeriodicWorkPolicy.KEEP,
+//            request
+//        )
+//    }
+
+//    fun cancelFetchingData() {
+//        FetchStandingsWorker.cancel(context)
+//    }
+
+//    fun fetchData() {
+//        viewModelScope.launch {
+//            val cachedData = getCachedStandingsUseCase.invoke(39,2019)
+//            cachedData?.let { data->
+//                _uiState.update { it.copy(response = data, isLoading = false)  }
+//            }
+//        }
+//    }
 
 
 }
