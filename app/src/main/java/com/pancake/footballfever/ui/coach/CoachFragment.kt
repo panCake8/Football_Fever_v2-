4cdb4137485fd5ed8fe8ebbe5b71d1fded328099
Bassam <bassamouf@std.mans.edu.eg>
2023-06-20 14:45:39 +0300

    handle coach fragment extended base

--- app/src/main/java/com/pancake/footballfever/ui/coach/CoachFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/coach/CoachFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.pancake.footballfever.R
+import com.pancake.footballfever.databinding.FragmentCoachBinding
+import com.pancake.footballfever.ui.base.BaseFragment
 
 
-class CoachFragment : Fragment() {
+class CoachFragment : BaseFragment<FragmentCoachBinding,CoachViewModel>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_coach
+
+    override val viewModel: CoachViewModel by viewModels()
+
 
 
 }
