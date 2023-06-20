e8aff91e91ab662134044607f4e053d8234e6826
youssef <yr925344git@gmail.com>
2023-06-20 17:02:50 +0300

    add Ui state

---
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/ui_state/TopScorerUiState.kt
@@ -0,0 +1,9 @@
+package com.pancake.footballfever.ui.league_state.top_score.ui_state
+
+import com.pancake.footballfever.domain.models.TopGoals
+
+data class TopScorerUiState(
+    val response: List<TopGoals>? = null,
+    val isLoading: Boolean = false,
+    val error: List<Error> = emptyList()
+)
