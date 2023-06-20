1f6cc05000530ee93991ecfb93e8340412767795
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-20 20:40:26 +0300

    update SearchFragment

--- app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
@@ -1,13 +1,18 @@
 package com.pancake.footballfever.ui.search
 
-import android.os.Bundle
-import android.view.View
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentSearchBinding
 import com.pancake.footballfever.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     override val layoutId: Int= R.layout.fragment_search
@@ -15,6 +20,22 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     override fun setup() {
         binding.searchRecycler.adapter=SearchAdapter(viewModel)
+        doSearch()
+    }
+
+    private fun doSearch() {
+        lifecycleScope.launch {
+            callbackFlow {
+                binding.searchBar.doOnTextChanged { text, _, _, _ ->
+                    text?.let { channel.trySend(it.toString()).isSuccess }
+                }
+                awaitClose()
+            }
+                .debounce(1_000)
+                .collect {
+                    viewModel.getDataBySearchText()
+                }
+        }
     }
 
 
