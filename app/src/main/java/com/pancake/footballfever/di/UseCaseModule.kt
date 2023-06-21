7d85eb4cfe0192f6da46f4e9ec9c218162aafcb8
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-21 15:58:20 +0300

    update Use Case module for dependency injection

--- app/src/main/java/com/pancake/footballfever/di/UseCaseModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/UseCaseModule.kt
@@ -2,12 +2,16 @@ package com.pancake.footballfever.di
 
 import com.pancake.footballfever.data.repository.search.CoachSearchRepository
 import com.pancake.footballfever.data.repository.search.LeagueSearchRepository
+import com.pancake.footballfever.data.repository.search.SearchKeywordsRepository
 import com.pancake.footballfever.data.repository.search.TeamSearchRepository
 import com.pancake.footballfever.domain.mappers.search.CoachSearchMapper
+import com.pancake.footballfever.domain.mappers.search.KeywordEntityToKeywordMapper
+import com.pancake.footballfever.domain.mappers.search.KeywordToKeywordEntityMapper
 import com.pancake.footballfever.domain.mappers.search.LeagueSearchMapper
 import com.pancake.footballfever.domain.mappers.search.TeamSearchMapper
 import com.pancake.footballfever.domain.usecase.GetCoachSearchUseCase
 import com.pancake.footballfever.domain.usecase.GetLeagueSearchUseCase
+import com.pancake.footballfever.domain.usecase.GetSearchKeywordsUseCase
 import com.pancake.footballfever.domain.usecase.GetTeamSearchUseCase
 import dagger.Module
 import dagger.Provides
@@ -46,5 +50,15 @@ object UseCaseModule {
     ) = GetCoachSearchUseCase(coachSearchRepository,
         coachSearchMapper,
     )
+
+    @Singleton
+    @Provides
+    fun provideSearchKeywordsUseCase(
+      searchKeywordsRepository: SearchKeywordsRepository,
+      keywordEntityToKeywordMapper: KeywordEntityToKeywordMapper,
+      keywordToKeywordEntityMapper: KeywordToKeywordEntityMapper
+    ) = GetSearchKeywordsUseCase(searchKeywordsRepository
+        ,keywordEntityToKeywordMapper,
+        keywordToKeywordEntityMapper)
 
 }
