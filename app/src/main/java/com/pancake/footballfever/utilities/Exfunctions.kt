61d98cce006e10846a6509cfdc4f10113f51c000
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-21 14:08:44 +0300

    add extension function toStandings

--- app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
@@ -2,6 +2,7 @@ package com.pancake.footballfever.utilities
 
 import com.example.footboolfever.data.remote.dto.standings.StandingsDto
 import com.pancake.footballfever.data.local.database.entity.StandingsEntity
+import com.pancake.footballfever.domain.models.Standings
 
 fun StandingsDto.toStandingsEntity() :List<StandingsEntity>? {
 
@@ -21,3 +22,14 @@ fun StandingsDto.toStandingsEntity() :List<StandingsEntity>? {
     }
 }
 
+fun StandingsEntity.toStandings(): Standings {
+    return Standings(
+        rank = rank,
+        teamName = teamName,
+        teamLogoUrl = teamLogoUrl,
+        teamId = teamId,
+        points = points,
+        win = win,
+        played = played
+    )
+}
