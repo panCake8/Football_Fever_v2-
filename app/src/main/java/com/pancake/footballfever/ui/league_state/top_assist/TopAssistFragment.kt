5ce9c9d12b0f56c21684f21157a12fbcdbbd1985
youssef <yr925344git@gmail.com>
2023-06-21 19:34:39 +0300

    top assist ui layer

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_assist/TopAssistFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_assist/TopAssistFragment.kt
@@ -1,9 +1,24 @@
 package com.pancake.footballfever.ui.league_state.top_assist
 
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.pancake.footballfever.R
+import com.pancake.footballfever.databinding.FragmentTopAssistBinding
+import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.league_state.top_assist.adapter.TopAssistAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
 
-class TopAssistFragment : Fragment() {
+@AndroidEntryPoint
+class TopAssistFragment : BaseFragment<FragmentTopAssistBinding, TopAssistViewModel>() {
 
+    override val layoutId: Int =  R.layout.fragment_top_assist
+    override val viewModel: TopAssistViewModel by viewModels()
+
+    override fun setup() {
+        super.setup()
+
+        val adapter = TopAssistAdapter(viewModel)
+        binding.recyclerViewAssist.adapter = adapter
+    }
 
 }
