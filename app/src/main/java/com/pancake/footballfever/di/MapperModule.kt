71fcd8b4d046469b1d706a342c4c2205a4c12dca
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-21 15:59:45 +0300

    update MapperModule for dependency injection

--- app/src/main/java/com/pancake/footballfever/di/MapperModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/MapperModule.kt
@@ -1,6 +1,8 @@
 package com.pancake.footballfever.di
 
 import com.pancake.footballfever.domain.mappers.search.CoachSearchMapper
+import com.pancake.footballfever.domain.mappers.search.KeywordEntityToKeywordMapper
+import com.pancake.footballfever.domain.mappers.search.KeywordToKeywordEntityMapper
 import com.pancake.footballfever.domain.mappers.search.LeagueSearchMapper
 import com.pancake.footballfever.domain.mappers.search.TeamSearchMapper
 import dagger.Module
@@ -24,6 +26,14 @@ object MapperModule {
     @Singleton
     @Provides
     fun provideCoachSearchMapper() = CoachSearchMapper()
+
+    @Singleton
+    @Provides
+    fun provideKeywordEntityToKeywordMapper() = KeywordEntityToKeywordMapper()
+
+    @Singleton
+    @Provides
+    fun provideKeywordToKeywordEntityMapper() = KeywordToKeywordEntityMapper()
 
 
 }
