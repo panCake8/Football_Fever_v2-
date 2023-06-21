54cf17ab253228067aa021fffd2625639bc53d89
youssef <yr925344git@gmail.com>
2023-06-21 14:16:09 +0300

    remove ViewModelScoped sellyme :))))

--- app/src/main/java/com/pancake/footballfever/data/local/mappers/playerMapper/TopScorerMapper.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/mappers/playerMapper/TopScorerMapper.kt
@@ -3,10 +3,8 @@ package com.pancake.footballfever.data.local.mappers.playerMapper
 import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
 import com.example.footboolfever.data.remote.dto.TopScoresDto
 import com.pancake.footballfever.data.local.mappers.Mapper
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class TopScorerMapper @Inject constructor(): Mapper<TopScoresDto, TopGoalsEntity> {
     override fun map(input: TopScoresDto): TopGoalsEntity {
         return TopGoalsEntity(
