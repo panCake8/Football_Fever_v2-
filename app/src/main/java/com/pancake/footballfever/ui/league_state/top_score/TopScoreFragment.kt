02eeb2466435cba17cf65f246beb8b1060637365
youssef <yr925344git@gmail.com>
2023-06-21 14:20:36 +0300

    add adapter to top scorer fragment

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScoreFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScoreFragment.kt
@@ -4,12 +4,20 @@ import androidx.fragment.app.viewModels
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentTopScoreBinding
 import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.league_state.top_score.adapter.TopScorerAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TopScoreFragment : BaseFragment<FragmentTopScoreBinding, TopScorerViewModel>() {
 
     override val layoutId: Int = R.layout.fragment_top_score
     override val viewModel: TopScorerViewModel by viewModels()
 
+    override fun setup() {
+        super.setup()
 
+        val adapter = TopScorerAdapter(viewModel)
+        binding.recyclerViewScorers.adapter = adapter
+    }
 
 }
