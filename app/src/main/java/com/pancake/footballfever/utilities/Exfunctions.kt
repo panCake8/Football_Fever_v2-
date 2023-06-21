db513f8cfb07acebc582fe4203bbdff8034f66b2
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-21 14:07:49 +0300

    add extension function toStandingsEntity

--- app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
@@ -1,2 +1,23 @@
 package com.pancake.footballfever.utilities
 
+import com.example.footboolfever.data.remote.dto.standings.StandingsDto
+import com.pancake.footballfever.data.local.database.entity.StandingsEntity
+
+fun StandingsDto.toStandingsEntity() :List<StandingsEntity>? {
+
+    val standingsItemItem =league?.standings?.first()
+    return standingsItemItem?.map  {
+        StandingsEntity(
+            teamId = it?.team?.id!!,
+            leagueId = league?.id!!,
+            season =league.season!!,
+            rank =it.rank!!,
+            teamName =it.team.name!!,
+            teamLogoUrl =it.team.logo!!,
+            points =it.points!!,
+            win =it.all?.win!!,
+            played = it.all.played!!,
+        )
+    }
+}
+
