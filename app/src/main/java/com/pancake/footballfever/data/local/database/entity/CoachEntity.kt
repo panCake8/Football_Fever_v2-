d10b37650df0e75b40680d1ab58f5e87cb8d1294
Bassam <bassamouf@std.mans.edu.eg>
2023-06-21 19:03:34 +0300

    add coach entity to coach model mapper

--- app/src/main/java/com/pancake/footballfever/data/local/database/entity/CoachEntity.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/database/entity/CoachEntity.kt
@@ -2,6 +2,7 @@ package com.pancake.footballfever.data.local.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.pancake.footballfever.domain.models.Coaches
 
 @Entity(tableName = "COACHES_TABLE")
 data class CoachEntity(
@@ -11,3 +12,11 @@ data class CoachEntity(
     val teamName: String? = "",
     val photo: String? = "",
 )
+
+fun CoachEntity.toCoachModel(): Coaches {
+    return Coaches(
+        name = name,
+        teamName = teamName,
+        photo = photo,
+    )
+}
