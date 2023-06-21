8681f4290aa2b1490b8b09c4135d3964d147f4bc
youssef <yr925344git@gmail.com>
2023-06-21 16:26:14 +0300

    add ex Functions

--- app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
@@ -1,2 +1,14 @@
 package com.pancake.footballfever.utilities
 
+import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
+import com.pancake.footballfever.domain.models.TopGoals
+
+fun TopGoalsEntity.toTopGoals(): TopGoals {
+    return TopGoals(
+        id = this.id,
+        playerName = this.playerName,
+        teamName = this.teamName,
+        totalGoals = this.totalGoals,
+        playerImg = this.playerImg,
+    )
+}
