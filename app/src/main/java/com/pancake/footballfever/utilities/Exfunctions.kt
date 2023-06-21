3206bc4cd3b7a4ba99e30206d0300322c6fdb7c8
youssef <yr925344git@gmail.com>
2023-06-21 19:29:41 +0300

    update Exfunctions

--- app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
@@ -1,6 +1,8 @@
 package com.pancake.footballfever.utilities
 
+import com.pancake.footballfever.data.local.database.entity.TopAssistEntity
 import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
+import com.pancake.footballfever.domain.models.TopAssists
 import com.pancake.footballfever.domain.models.TopGoals
 
 fun TopGoalsEntity.toTopGoals(): TopGoals {
@@ -9,6 +11,16 @@ fun TopGoalsEntity.toTopGoals(): TopGoals {
         playerName = this.playerName,
         teamName = this.teamName,
         totalGoals = this.totalGoals,
+        playerImg = this.playerImg,
+    )
+}
+
+fun TopAssistEntity.toTopAssist(): TopAssists {
+    return TopAssists(
+        id = this.id,
+        playerName = this.playerName,
+        teamName = this.teamName,
+        totalAssists = this.totalAssists,
         playerImg = this.playerImg,
     )
 }
