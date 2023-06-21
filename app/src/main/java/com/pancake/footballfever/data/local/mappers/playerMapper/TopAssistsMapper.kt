b5950f189eef706a4d0594f374dd0b844d31affa
youssef <yr925344git@gmail.com>
2023-06-21 19:29:25 +0300

    add Top assist mapper

--- app/src/main/java/com/pancake/footballfever/data/local/mappers/playerMapper/TopAssistsMapper.kt
+++ app/src/main/java/com/pancake/footballfever/data/local/mappers/playerMapper/TopAssistsMapper.kt
@@ -1,12 +1,10 @@
 package com.pancake.footballfever.data.local.mappers.playerMapper
 
-import com.pancake.footballfever.data.local.database.entity.TopAssistEntity
 import com.example.footboolfever.data.remote.dto.TopAssistsDto
+import com.pancake.footballfever.data.local.database.entity.TopAssistEntity
 import com.pancake.footballfever.data.local.mappers.Mapper
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class TopAssistsMapper @Inject constructor(): Mapper<TopAssistsDto, TopAssistEntity> {
 
     override fun map(input: TopAssistsDto): TopAssistEntity {
