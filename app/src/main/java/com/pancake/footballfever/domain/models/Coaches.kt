6afd8e8df024a78e6e4b48ae5dddaee139fac13c
Bassam <bassamouf@std.mans.edu.eg>
2023-06-21 18:33:25 +0300

    set default values

--- app/src/main/java/com/pancake/footballfever/domain/models/Coaches.kt
+++ app/src/main/java/com/pancake/footballfever/domain/models/Coaches.kt
@@ -2,7 +2,7 @@ package com.pancake.footballfever.domain.models
 
 
 data class Coaches(
-    val name: String,
-    val teamName: String,
-    val photo: String,
+    val name: String? = "",
+    val teamName: String? = "",
+    val photo: String? = "",
 )
