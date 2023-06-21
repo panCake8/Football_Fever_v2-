739ca061e1047ea97972bdcd72bd5050ea1a9fe3
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-21 15:57:11 +0300

    update SearchFragment for caching the keywords

--- app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/search/SearchFragment.kt
@@ -1,10 +1,14 @@
 package com.pancake.footballfever.ui.search
 
+import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.chip.Chip
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentSearchBinding
+import com.pancake.footballfever.domain.models.SearchKeyword
 import com.pancake.footballfever.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.channels.awaitClose
@@ -20,24 +24,57 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     override fun setup() {
         binding.searchRecycler.adapter=SearchAdapter(viewModel)
-        doSearch()
+
+           lifecycleScope.launch {
+               viewModel.searchKeyword.collect { searchKeywords ->
+                   Log.i("Abdallahx3x", searchKeywords.toString())
+                   makeSuggestKeywordToChips(searchKeywords)
+               }
+           }
+
+        searchViewListener()
     }
 
-    private fun doSearch() {
+    private fun searchViewListener() {
         lifecycleScope.launch {
             callbackFlow {
                 binding.searchBar.doOnTextChanged { text, _, _, _ ->
                     text?.let { channel.trySend(it.toString()).isSuccess }
+                    if (text.isNullOrEmpty()) {
+                        viewModel.showKeywordSuggests()
+                        Log.i("x3x",viewModel.searchKeyword.value.toString())
+                    }
                 }
                 awaitClose()
             }
                 .debounce(1_000)
-                .collect {
-                    viewModel.getDataBySearchText()
+                .collect {text->
+                       makeSearch(text)
                 }
         }
     }
 
+    private  fun  makeSearch(text:String){
+        if (text.isNotBlank()) {
+            viewModel.cacheKeyword(text)
+            viewModel.getDataBySearchText()
+        }
+    }
 
+    private fun makeSuggestKeywordToChips(items: List<SearchKeyword>?) {
+        binding.searchSuggest.removeAllViews()
+        items?.take(8)?.forEach {searchKeyword->
+            val chip = Chip(context).apply {
+                text = searchKeyword.keyword
+                isCloseIconVisible = false
+                setTextColor(ContextCompat.getColor(context, R.color.black))
+                setOnClickListener{
+                    binding.searchBar.setText(searchKeyword.keyword)
+                    makeSearch(searchKeyword.keyword!!)
+                }
+            }
+            binding.searchSuggest.addView(chip)
+        }
+    }
 
 }
