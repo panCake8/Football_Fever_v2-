94868e7b6a0b13e3f3f1547b2bc62976423e5458
youssef <yr925344git@gmail.com>
2023-06-21 14:19:57 +0300

    update top scorer view model

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScorerViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/TopScorerViewModel.kt
@@ -2,15 +2,17 @@ package com.pancake.footballfever.ui.league_state.top_score
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pancake.footballfever.domain.usecases.FetchTopGoalsUseCase
-import com.pancake.footballfever.domain.usecases.GetTopGoalsCachedDataUseCase
+import com.pancake.footballfever.domain.usecase.FetchTopGoalsUseCase
+import com.pancake.footballfever.domain.usecase.GetTopGoalsCachedDataUseCase
 import com.pancake.footballfever.ui.league_state.top_score.adapter.TopScorerListener
 import com.pancake.footballfever.ui.league_state.top_score.ui_state.TopScorerUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,35 +25,23 @@ class TopScorerViewModel @Inject constructor(
     val uiState: StateFlow<TopScorerUiState> = _uiState
 
     init {
+        fetchData(39, 2022)
+    }
+
+
+    private fun fetchData(leagues: Int, seasons: Int) {
         viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val result = fetchTopGoalsUseCase(leagues, seasons)
+                if (result.isSuccess) {
+                    _uiState.update { it.copy(isLoading = true) }
+                    _uiState.update { it.copy(response = getTopGoalsCachedDataUseCase(), isLoading = false) }
+                } else if (result.isFailure) {
+                    _uiState.update { it.copy(error = true) }
+                }
+            }
 
         }
     }
-
-    private fun showTopScorer(league: Int, season: Int) {
-        viewModelScope.launch {
-
-        }
-    }
-//    fun fetchData(leagues: Int, seasons: Int) {
-//        viewModelScope.launch {
-//            val result = fetchTopGoalsUseCase(leagues, seasons)
-//            if (result.isSuccess) {
-//                _uiState.update { it.copy(isLoading = true) }
-//                _uiState.update { it.copy(response = getAllScorerUseCase.invoke(league, season), isLoading = false) }
-//            } else if (result.isFailure) {
-//                // do something else
-//            }
-//        }
-//    }
-//
-//    fun getCachedDataData() {
-//        viewModelScope.launch {
-//            val cachedData = getTopGoalsCachedDataUseCase()
-//            cachedData?.let {
-//                // give it the value :)
-//            }
-//        }
-//    }
 
 }
