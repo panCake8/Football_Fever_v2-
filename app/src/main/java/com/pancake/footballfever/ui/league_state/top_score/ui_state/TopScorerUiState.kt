4cc3db79b046af9c87ec05b31492fc70240f9bbf
youssef <yr925344git@gmail.com>
2023-06-21 16:28:29 +0300

    update top scorer ui state

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/ui_state/TopScorerUiState.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/ui_state/TopScorerUiState.kt
@@ -3,7 +3,7 @@ package com.pancake.footballfever.ui.league_state.top_score.ui_state
 import com.pancake.footballfever.domain.models.TopGoals
 
 data class TopScorerUiState(
-    val response: List<TopGoals>? = null,
+    val topGoalsList: List<TopGoals>? = null,
     val isLoading: Boolean = false,
     val error: Boolean = false,
 )
