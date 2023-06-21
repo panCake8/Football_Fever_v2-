3f4d6e7a2d1eb756556fbeed893cc7a14ab2d5d7
youssef <yr925344git@gmail.com>
2023-06-21 14:09:35 +0300

    add extension class

---
+++ app/src/main/java/com/pancake/footballfever/domain/usecase/extension.kt
@@ -0,0 +1,14 @@
+package com.pancake.footballfever.domain.usecase
+
+import com.pancake.footballfever.data.local.database.entity.TopGoalsEntity
+import com.pancake.footballfever.domain.models.TopGoals
+
+fun TopGoalsEntity.toTopGoals(): TopGoals {
+    return TopGoals(
+        id = this.id,
+        playerName = this.playerName,
+        teamName = this.teamName,
+        totalGoals = this.totalGoals,
+        playerImg = this.playerImg,
+    )
+}
