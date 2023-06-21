94b071293a3e2f1ec5f717036f65801e9b760b8c
youssef <yr925344git@gmail.com>
2023-06-21 14:13:19 +0300

    update top scorer adapter

--- app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/adapter/TopScorerAdapter.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/top_score/adapter/TopScorerAdapter.kt
@@ -1,10 +1,11 @@
 package com.pancake.footballfever.ui.league_state.top_score.adapter
 
+import com.pancake.footballfever.R
 import com.pancake.footballfever.domain.models.TopGoals
 import com.pancake.footballfever.ui.base.BaseAdapter
 
 
 class TopScorerAdapter(listener: TopScorerListener) : BaseAdapter<TopGoals>(listener) {
     override val getLayoutId: Int
-        get() = TODO("Not yet implemented")
+        get() = R.layout.item_goal_scorer
 }
