ed4eb396d58aea528591b5882661fc7ff40add34
kareem <skrym826@gmail.com>
2023-06-22 00:22:16 +0300

    add extension function to convert timestamp to date

--- app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
+++ app/src/main/java/com/pancake/footballfever/utilities/Exfunctions.kt
@@ -1,2 +1,15 @@
 package com.pancake.footballfever.utilities
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+
+fun Int.toDate(): String {
+    val calender = Calendar.getInstance()
+    calender.timeInMillis = this.toLong()
+    val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+    return format.format(calender.time)
+
+
+}
