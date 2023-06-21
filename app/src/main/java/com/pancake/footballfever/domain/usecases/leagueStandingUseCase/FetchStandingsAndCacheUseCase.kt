102319525e420b473e2d5525485395a364ff8005
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-21 14:10:09 +0300

    change location file

--- app/src/main/java/com/pancake/footballfever/domain/usecases/leagueStandingUseCase/FetchStandingsAndCacheUseCase.kt
+++ app/src/main/java/com/pancake/footballfever/domain/usecases/leagueStandingUseCase/FetchStandingsAndCacheUseCase.kt
@@ -1,4 +1,4 @@
-package com.pancake.footballfever.domain.usecases
+package com.pancake.footballfever.domain.usecases.leagueStandingUseCase
 
 import com.pancake.footballfever.data.repository.StandingsRepository
 import javax.inject.Inject
