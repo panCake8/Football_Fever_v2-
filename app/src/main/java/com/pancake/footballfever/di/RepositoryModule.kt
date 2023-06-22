979e0d680080a4d6b86bc27319e846fd8f33bfca
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-22 12:50:22 +0300

    solve conflicts

--- app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
+++ app/src/main/java/com/pancake/footballfever/di/RepositoryModule.kt
@@ -13,6 +13,14 @@ import com.pancake.footballfever.data.repository.StandingsRepository
 import com.pancake.footballfever.data.repository.StandingsRepositoryImp
 import com.pancake.footballfever.data.repository.TeamRepository
 import com.pancake.footballfever.data.repository.TeamRepositoryImpl
+import com.pancake.footballfever.data.repository.search.CoachSearchRepository
+import com.pancake.footballfever.data.repository.search.CoachSearchRepositoryImpl
+import com.pancake.footballfever.data.repository.search.LeagueSearchRepository
+import com.pancake.footballfever.data.repository.search.LeagueSearchRepositoryImpl
+import com.pancake.footballfever.data.repository.search.SearchKeywordsRepository
+import com.pancake.footballfever.data.repository.search.SearchKeywordsRepositoryImpl
+import com.pancake.footballfever.data.repository.search.TeamSearchRepository
+import com.pancake.footballfever.data.repository.search.TeamSearchRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -58,4 +66,29 @@ abstract class RepositoryModule {
     abstract fun bindsFixtureRepository(
         fixtureRepositoryImpl: FixtureRepositoryImpl
     ): FixtureRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsTeamSearchRepository(
+        teamSearchRepositoryImpl: TeamSearchRepositoryImpl
+    ): TeamSearchRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsLeagueSearchRepository(
+        leagueSearchRepositoryImpl: LeagueSearchRepositoryImpl
+    ): LeagueSearchRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsCoachSearchRepository(
+        coachSearchRepositoryImpl: CoachSearchRepositoryImpl
+    ): CoachSearchRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsSearchKeywordsRepository(
+        searchKeywordsRepositoryImpl: SearchKeywordsRepositoryImpl
+    ): SearchKeywordsRepository
+
 }
