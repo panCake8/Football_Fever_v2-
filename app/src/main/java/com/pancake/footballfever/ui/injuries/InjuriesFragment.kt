2b88e81f465aff85506033479cf704fefe8655b5
Bassam <bassamouf@std.mans.edu.eg>
2023-06-22 18:08:12 +0300

    add injuries fragment logic

--- app/src/main/java/com/pancake/footballfever/ui/injuries/InjuriesFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/injuries/InjuriesFragment.kt
@@ -1,60 +1,32 @@
 package com.pancake.footballfever.ui.injuries
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.pancake.footballfever.R
+import com.pancake.footballfever.databinding.FragmentInjuriesBinding
+import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.injuries.adapter.InjuriesAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class InjuriesFragment : BaseFragment<FragmentInjuriesBinding,InjuriesViewModel>() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InjuriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class InjuriesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override val layoutId: Int = R.layout.fragment_injuries
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override val viewModel: InjuriesViewModel by viewModels()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_injuries, container, false)
+
+    private fun setupRecyclerView() {
+        binding.injuriesRecyclerView.adapter = InjuriesAdapter(viewModel)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InjuriesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InjuriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
