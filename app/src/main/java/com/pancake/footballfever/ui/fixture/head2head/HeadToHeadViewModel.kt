f76698bdd24225796e0b7e0bc96327f6552f139b
kareem <skrym826@gmail.com>
2023-06-23 00:49:04 +0300

    add HeadToHead fragment and ViewModel

---
+++ app/src/main/java/com/pancake/footballfever/ui/fixture/head2head/HeadToHeadViewModel.kt
@@ -0,0 +1,51 @@
+package com.pancake.footballfever.ui.fixture.head2head
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pancake.footballfever.domain.models.HeadToHead
+import com.pancake.footballfever.domain.usecase.GetHeadToHeadsUseCase
+import com.pancake.footballfever.ui.fixture.head2head.adapter.HeadToHeadListener
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeadToHeadViewModel @Inject
+constructor(
+    private val getHeadToHeadsUseCase: GetHeadToHeadsUseCase,
+) : ViewModel(), HeadToHeadListener {
+
+    private val _HeadToHeads = MutableStateFlow(HeadToHeadUiState())
+
+    val headToHead: StateFlow<HeadToHeadUiState>
+        get() = _HeadToHeads
+
+    init {
+        getHeadToHeads("33-34")
+    }
+
+
+    private fun getHeadToHeads(h2h: String) {
+        viewModelScope.launch {
+            getHeadToHeadsUseCase.getHeadToHeads(h2h).let { list ->
+                _HeadToHeads.update {
+                    it.copy(
+                        isLoading = false,
+                        success = list
+                    )
+                }
+                Log.i("TAG", "${list?.size}")
+            }
+        }
+
+    }
+
+
+    override fun onClick(headToHead: HeadToHead) {
+
+    }
+}
