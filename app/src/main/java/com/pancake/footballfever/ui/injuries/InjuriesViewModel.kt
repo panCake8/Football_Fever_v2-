61b3294b3b7b0ff3b572b724af902f81eaca7e1a
Bassam <bassamouf@std.mans.edu.eg>
2023-06-22 18:07:44 +0300

    add injuries view model

---
+++ app/src/main/java/com/pancake/footballfever/ui/injuries/InjuriesViewModel.kt
@@ -0,0 +1,69 @@
+package com.pancake.footballfever.ui.injuries
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pancake.footballfever.data.local.database.entity.toInjuriesModel
+import com.pancake.footballfever.domain.models.Injuries
+import com.pancake.footballfever.domain.usecases.FetchInjuriesUseCase
+import com.pancake.footballfever.domain.usecases.GetInjuriesUseCase
+import com.pancake.footballfever.ui.injuries.adapter.InjuriesListener
+import com.pancake.footballfever.utilities.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class InjuriesViewModel @Inject constructor(
+    private val fetchInjuriesUseCase: FetchInjuriesUseCase,
+    private val getInjuriesUseCase: GetInjuriesUseCase,
+): ViewModel(), InjuriesListener {
+
+
+    private val _injury = MutableStateFlow(InjuriesUiState())
+    val injury: StateFlow<InjuriesUiState>
+        get() = _injury
+
+    val injuryEvent = MutableLiveData<Event<Injuries>>()
+
+    init {
+        fetchInjuries()
+    }
+
+    fun fetchInjuries() {
+        viewModelScope.launch {
+            try {
+                val injury = fetchInjuriesUseCase()
+                _injury.update {
+                    it.copy(
+                        isLoading = false,
+                        success = injury.getOrNull()!!.map { it.toInjuriesModel() }
+                    )
+                }
+            } catch (e: Throwable) {
+                _injury.update { it.copy(error = e.message) }
+            }
+        }
+        getInjuries()
+    }
+
+    private fun getInjuries(){
+        viewModelScope.launch{
+            val coach = getInjuriesUseCase()
+            _injury.update {
+                it.copy(
+                    isLoading = false,
+                    success = coach
+                )
+            }
+        }
+    }
+
+    override fun onClickInjury(injuries: Injuries) {
+        injuryEvent.postValue(Event(injuries))
+    }
+
+}
