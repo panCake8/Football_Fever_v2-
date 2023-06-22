4f3ccc02108837153aaa69e59ced4e6b280f539c
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2023-06-22 15:58:44 +0300

    implement args in adapter and fragment

--- app/src/main/java/com/pancake/footballfever/ui/league_state/match/standing/MatchStandingAdapter.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/match/standing/MatchStandingAdapter.kt
@@ -1,11 +1,32 @@
 package com.pancake.footballfever.ui.league_state.match.standing
 
+import androidx.core.content.ContextCompat
 import com.pancake.footballfever.R
+import com.pancake.footballfever.databinding.ItemStandingsBinding
 import com.pancake.footballfever.domain.models.Standings
 import com.pancake.footballfever.ui.base.BaseAdapter
 
-class MatchStandingAdapter(listener: MatchStandingListener) : BaseAdapter<Standings>(listener) {
+class MatchStandingAdapter(
+    listener: MatchStandingListener,
+    private val homeTeamId: Int,
+    private val awayTeamId: Int
+) : BaseAdapter<Standings>(listener) {
     override val getLayoutId: Int
         get() = R.layout.item_standings
 
+    override fun bind(holder: ItemViewHolder, position: Int) {
+        super.bind(holder, position)
+        val currentItem = getItem(position)
+        (holder.binding as ItemStandingsBinding).apply {
+
+            val primaryColor = ContextCompat.getColor(root.context, R.color.Primary_color)
+            val secondryColor = ContextCompat.getColor(root.context, R.color.Secondary_color)
+
+            itemRecycle.setBackgroundColor(
+                if (currentItem.teamId == homeTeamId ||currentItem.teamId == awayTeamId ) secondryColor
+                else primaryColor
+            )
+
+        }
+    }
 }
--- app/src/main/java/com/pancake/footballfever/ui/league_state/match/standing/MatchStandingFragment.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/match/standing/MatchStandingFragment.kt
@@ -1,6 +1,7 @@
 package com.pancake.footballfever.ui.league_state.match.standing
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentMatchStandingBinding
 import com.pancake.footballfever.ui.base.BaseFragment
@@ -10,14 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MatchStandingFragment : BaseFragment<FragmentMatchStandingBinding, MatchStandingViewModel>() {
     override val layoutId = R.layout.fragment_match_standing
     override val viewModel : MatchStandingViewModel by viewModels ()
-
+    private val argument : MatchStandingFragmentArgs by navArgs()
     override fun setup() {
         super.setup()
 
-        val adapter = MatchStandingAdapter(viewModel)
+        val adapter = MatchStandingAdapter(viewModel , argument.homeTeamId, argument.awayTeamId)
         binding.recyclerStandingLeague.adapter = adapter
     }
-
-
 
 }
