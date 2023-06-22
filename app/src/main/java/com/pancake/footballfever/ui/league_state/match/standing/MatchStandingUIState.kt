23722126b9c5e4cd82ee2fc49cab82e4ae13442b
Yassen Ramadan <118762446+yassenRamadan1@users.noreply.github.com>
2023-06-22 21:38:47 +0300

    Revert "Feature/match stats standing screan"

--- app/src/main/java/com/pancake/footballfever/ui/league_state/match/standing/MatchStandingUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.pancake.footballfever.ui.league_state.match.standing
-
-import com.pancake.footballfever.domain.models.Standings
-
-data class MatchStandingUIState(
-    val response: List<Standings>? = null,
-    val isLoading: Boolean = true,
-    val errors: Error? =null
-)
