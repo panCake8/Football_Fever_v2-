c550ef655513eb413d1db54ca02dc820e0cec0b6
youssef <yr925344git@gmail.com>
2023-06-23 14:28:32 +0300

    update league repository

--- app/src/main/java/com/pancake/footballfever/data/repository/LeaguesRepository.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/LeaguesRepository.kt
@@ -1,7 +1,29 @@
 package com.pancake.footballfever.data.repository
 
-import com.example.footboolfever.data.remote.dto.league.LeaguesDto
+import com.pancake.footballfever.data.local.database.entity.LeagueEntity
+import com.pancake.footballfever.data.remote.dto.league.LeaguesDto
 
 interface LeaguesRepository {
     suspend fun getPremierLeagues(countryName : String) : List<LeaguesDto>
+
+    // region leagues
+
+    suspend fun getAllCurrentLeaguesRemote(current: Boolean): List<LeaguesDto>
+
+    suspend fun getAllCurrentLeaguesLocal(): List<LeagueEntity>
+
+    suspend fun insertAllLeagues(leagues:List<LeagueEntity>)
+
+    suspend fun deleteAllCurrentLeagues()
+
+    suspend fun fetchCurrentLeaguesDataAndCache(current: Boolean): Result<List<LeagueEntity>>
+
+    suspend fun getCurrentCachedData(): List<LeagueEntity>
+
+    // endregion
+
+    // region leagues state
+    suspend fun getLeaguesByIdLocally(leagueId: Int): LeagueEntity
+
+    // endregion
 }
--- app/src/main/java/com/pancake/footballfever/data/repository/LeaguesRepositoryImpl.kt
+++ app/src/main/java/com/pancake/footballfever/data/repository/LeaguesRepositoryImpl.kt
@@ -1,18 +1,59 @@
 package com.pancake.footballfever.data.repository
 
 
-import com.example.footboolfever.data.remote.dto.league.LeaguesDto
+
 import com.pancake.footballfever.data.local.database.daos.LeagueDao
+import com.pancake.footballfever.data.local.database.entity.LeagueEntity
+import com.pancake.footballfever.data.remote.dto.league.LeaguesDto
+import com.pancake.footballfever.data.remote.dto.league.toLeagueEntity
 import com.pancake.footballfever.data.remote.service.ApiService
 import javax.inject.Inject
 
 class LeaguesRepositoryImpl @Inject constructor(
     private val apiService: ApiService,
-    private val dao: LeagueDao
+    private val leagueDao: LeagueDao
 ) : LeaguesRepository {
 
-    override suspend fun getPremierLeagues(countryName: String): List<LeaguesDto> {
-        return apiService.getLeagueByCountryName(countryName).body()?.response!!
+    override suspend fun getPremierLeagues(countryName: String): List<LeaguesDto> = apiService.getLeagueByCountryName(countryName).body()?.response!!
+
+    override suspend fun getAllCurrentLeaguesRemote(current: Boolean): List<LeaguesDto> = apiService.getCurrentLeague(current).body()?.response!!
+
+
+    override suspend fun getAllCurrentLeaguesLocal(): List<LeagueEntity> {
+        return leagueDao.getAllLeagues()
     }
 
+    override suspend fun insertAllLeagues(leagues: List<LeagueEntity>) {
+        leagueDao.insertLeagues(leagues)
+    }
+
+    override suspend fun deleteAllCurrentLeagues() {
+        leagueDao.deleteAllLeagues()
+    }
+
+    override suspend fun fetchCurrentLeaguesDataAndCache(current: Boolean): Result<List<LeagueEntity>> {
+        return try {
+            val response = apiService.getCurrentLeague(current)
+            if (response.isSuccessful) {
+                val data = response.body()?.response?.map { it.toLeagueEntity() }
+                data?.let {
+                    leagueDao.deleteAllLeagues()
+                    leagueDao.insertLeagues(it)
+                    return Result.success(it)
+                }
+            }
+            Result.failure(Exception(response.message().toString()))
+        } catch (e: java.lang.Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun getCurrentCachedData(): List<LeagueEntity> {
+        return leagueDao.getAllLeagues()
+    }
+
+    // region league state screen
+    override suspend fun getLeaguesByIdLocally(leagueId: Int): LeagueEntity = leagueDao.getLeagueById(leagueId)!!
+
+    // endregion
 }
