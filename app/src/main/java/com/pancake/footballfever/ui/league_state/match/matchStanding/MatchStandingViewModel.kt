5d72d17d03f60ce0f37985f00bd276db7f92800f
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2023-06-23 16:45:10 +0300

    add save state handle to view model

--- app/src/main/java/com/pancake/footballfever/ui/league_state/match/matchStanding/MatchStandingViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/league_state/match/matchStanding/MatchStandingViewModel.kt
@@ -1,5 +1,6 @@
 package com.pancake.footballfever.ui.league_state.match.matchStanding
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pancake.footballfever.domain.usecases.leagueStandingUseCase.FetchStandingsAndCacheUseCase
@@ -12,26 +13,33 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
+private const val HOME_TEAM_ID = "homeTeamId"
+private const val AWAY_TEAM_ID = "awayTeamId"
+private const val SEASON = "season"
 @HiltViewModel
 class MatchStandingViewModel @Inject constructor(
     private val fetchStandingsAndCacheUseCase: FetchStandingsAndCacheUseCase,
     private val getCachedStandingsUseCase: GetCachedStandingsUseCase,
-
-
-    ) : ViewModel(), MatchStandingListener {
+    savedStateHandle: SavedStateHandle
+) : ViewModel(), MatchStandingListener {
     private val _uiState = MutableStateFlow(MatchStandingUIState())
     val uiState: StateFlow<MatchStandingUIState> = _uiState
+
+    val homeTeamId = savedStateHandle.get<Int>(HOME_TEAM_ID)!!
+    val awayTeamId = savedStateHandle.get<Int>(AWAY_TEAM_ID)!!
+    val season = savedStateHandle.get<Int>(SEASON)!!
+
     init {
+
 
         viewModelScope.launch(Dispatchers.IO) {
 
-            fetchStandingsAndCacheUseCase.invoke(39, 2019)
+             fetchStandingsAndCacheUseCase.invoke(homeTeamId,season)
 
             _uiState.update {
                 it.copy(
-                    standings = getCachedStandingsUseCase.invoke(39, 2019),
-                    isLoading = false
+                    isLoading = false,
+                    standings = getCachedStandingsUseCase.invoke(homeTeamId, season),
                 )
             }
 
