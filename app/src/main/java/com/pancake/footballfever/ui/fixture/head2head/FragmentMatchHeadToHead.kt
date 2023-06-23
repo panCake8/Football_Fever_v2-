287646586c014d0148e006a212737f392f1ea308
youssef <yr925344git@gmail.com>
2023-06-23 20:56:50 +0300

    add h2h

--- app/src/main/java/com/pancake/footballfever/ui/fixture/head2head/FragmentMatchHeadToHead.kt
+++ app/src/main/java/com/pancake/footballfever/ui/fixture/head2head/FragmentMatchHeadToHead.kt
@@ -2,9 +2,11 @@ package com.pancake.footballfever.ui.fixture.head2head
 
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentMatchStatsHeadToHeadBinding
 import com.pancake.footballfever.ui.base.BaseFragment
+import com.pancake.footballfever.ui.fixture.FixtureFragmentArgs
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val HOME_ID_ARG = "home_id_arg"
@@ -15,6 +17,16 @@ class FragmentMatchHeadToHead :
     override val layoutId: Int
         get() = R.layout.fragment_match_stats_head_to_head
     override val viewModel: HeadToHeadViewModel by viewModels()
+
+
+
+    override fun setup() {
+
+        val teamIds = arguments?.getString(HOME_ID_ARG)
+        teamIds?.let {
+            viewModel.getHeadToHeads(it)
+        }
+    }
 
 
     companion object {
