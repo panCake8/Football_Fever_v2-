d6b99fb956243b0bfbebc04662637f72777a8580
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2023-06-23 16:53:29 +0300

    add save state handle to view model

--- app/src/main/java/com/pancake/footballfever/ui/club_standing/ClubStandingViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/club_standing/ClubStandingViewModel.kt
@@ -1,5 +1,6 @@
 package com.pancake.footballfever.ui.club_standing
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pancake.footballfever.domain.usecases.leagueStandingUseCase.FetchStandingsAndCacheUseCase
@@ -12,25 +13,29 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private const val HOME_TEAM_ID = "teamId"
+private const val SEASON = "season"
 @HiltViewModel
 class ClubStandingViewModel @Inject constructor(
     private val fetchStandingsAndCacheUseCase: FetchStandingsAndCacheUseCase,
     private val getCachedStandingsUseCase: GetCachedStandingsUseCase,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel(), ClubStandingListener {
     private val _uiState = MutableStateFlow(ClubStandingUIState())
     val uiState: StateFlow<ClubStandingUIState> = _uiState
-
+    val teamId = savedStateHandle.get<Int>(HOME_TEAM_ID)!!
+    val season = savedStateHandle.get<Int>(SEASON)!!
 
     init {
 
         viewModelScope.launch(Dispatchers.IO) {
 
-            fetchStandingsAndCacheUseCase.invoke(39, 2019)
+            fetchStandingsAndCacheUseCase.invoke(teamId, season)
 
             _uiState.update {
                 it.copy(
                     isLoading = false,
-                    standings = getCachedStandingsUseCase.invoke(39, 2019)
+                    standings = getCachedStandingsUseCase.invoke(teamId, season)
                 )
             }
 
