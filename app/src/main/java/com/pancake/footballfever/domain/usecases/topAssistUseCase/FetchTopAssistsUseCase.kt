c6bcc4003624f2450796a23263b0c97b79274dd5
youssef <yr925344git@gmail.com>
2023-06-23 14:17:19 +0300

    update player use case

--- app/src/main/java/com/pancake/footballfever/domain/usecases/topAssistUseCase/FetchTopAssistsUseCase.kt
+++ app/src/main/java/com/pancake/footballfever/domain/usecases/topAssistUseCase/FetchTopAssistsUseCase.kt
@@ -1,24 +1,11 @@
 package com.pancake.footballfever.domain.usecases.topAssistUseCase
 
 import com.pancake.footballfever.data.repository.playerRepo.PlayerRepository
-import com.pancake.footballfever.domain.models.TopAssists
-import com.pancake.footballfever.utilities.toTopAssist
 import javax.inject.Inject
 
 class FetchTopAssistsUseCase @Inject constructor(
     private val playerRepository: PlayerRepository,
 ) {
-
-    suspend operator fun invoke(league: Int, season: Int): Result<List<TopAssists>> {
-        return try {
-            val result = playerRepository.fetchTopAssistsDataAndCache(league, season)
-            val topAssistsList = result.getOrThrow()
-                .map {it.toTopAssist()}
-            Result.success(topAssistsList)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
+    suspend operator fun invoke(league: Int, season: Int) = playerRepository.fetchTopAssistsDataAndCache(league, season)
 
 }
--- app/src/main/java/com/pancake/footballfever/domain/usecases/topScorerUsecase/FetchTopGoalsUseCase.kt
+++ app/src/main/java/com/pancake/footballfever/domain/usecases/topScorerUsecase/FetchTopGoalsUseCase.kt
@@ -1,23 +1,11 @@
 package com.pancake.footballfever.domain.usecases.topScorerUsecase
 
 import com.pancake.footballfever.data.repository.playerRepo.PlayerRepository
-import com.pancake.footballfever.domain.models.TopGoals
-import com.pancake.footballfever.utilities.toTopGoals
 import javax.inject.Inject
 
 class FetchTopGoalsUseCase @Inject constructor(
     private val playerRepository: PlayerRepository,
 ) {
-
-    suspend operator fun invoke(league: Int, season: Int): Result<List<TopGoals>> {
-        return try {
-            val result = playerRepository.fetchTopGoalsDataAndCache(league, season)
-            val topGoalsList = result.getOrThrow()
-                .map { it.toTopGoals() }
-            Result.success(topGoalsList)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
+    suspend operator fun invoke(league: Int, season: Int) = playerRepository.fetchTopGoalsDataAndCache(league, season)
 
 }
