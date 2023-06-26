2d694643267086eb225574bdeea0ceeb2e349408
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-06-27 01:55:24 +0300

    add event class

--- app/src/main/java/com/pancake/footballfever/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/favourites/FavouritesViewModel.kt
@@ -25,21 +25,13 @@ class FavouritesViewModel @Inject constructor(
 
     val favouriteTeams: StateFlow<DataState<Any>> = _favouriteTeams
     init {
-        getAllFavouriteLocal()
-//        viewModelScope.launch {
-//                _favouriteTeams.value =
-//                    if(getAllFavouriteTeamsUseCase.getAllTeams().isNotEmpty())
-//                    DataState.Success(getAllFavouriteTeamsUseCase.getAllTeams())
-//                    else DataState.Error("error")
-//
-//
-//
-//
-//        }
-    }
-    private fun getAllFavouriteLocal(){
         viewModelScope.launch {
-            getAllFavouriteTeamsUseCase.getAllTeams()
+                _favouriteTeams.value =
+                    if(getAllFavouriteTeamsUseCase.getAllTeams().isNotEmpty())
+                    DataState.Success(getAllFavouriteTeamsUseCase.getAllTeams())
+                    else DataState.Error("error")
+
+
         }
     }
 
