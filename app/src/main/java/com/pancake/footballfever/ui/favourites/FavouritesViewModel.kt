4d7fe285c0eea43365c8e6249bbd99798174a83b
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-06-27 01:41:44 +0300

    update

--- app/src/main/java/com/pancake/footballfever/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/favourites/FavouritesViewModel.kt
@@ -26,16 +26,12 @@ class FavouritesViewModel @Inject constructor(
     val favouriteTeams: StateFlow<DataState<Any>> = _favouriteTeams
     init {
         getAllFavouriteLocal()
-//        viewModelScope.launch {
-//                _favouriteTeams.value =
-//                    if(getAllFavouriteTeamsUseCase.getAllTeams().isNotEmpty())
-//                    DataState.Success(getAllFavouriteTeamsUseCase.getAllTeams())
-//                    else DataState.Error("error")
-//
-//
-//
-//
-//        }
+        viewModelScope.launch {
+                _favouriteTeams.value =
+                    if(getAllFavouriteTeamsUseCase.getAllTeams().isNotEmpty())
+                    DataState.Success(getAllFavouriteTeamsUseCase.getAllTeams())
+                    else DataState.Error("error")
+        }
     }
     private fun getAllFavouriteLocal(){
         viewModelScope.launch {
