7ad29b05313cd040ee7f0776696c1328a4a3d42d
Bassam <bassamouf@std.mans.edu.eg>
2023-06-27 00:45:07 +0300

    add transfers entity

---
+++ app/src/main/java/com/pancake/footballfever/data/local/database/entity/TransferEntity.kt
@@ -0,0 +1,18 @@
+package com.pancake.footballfever.data.local.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "TRANSFER_TABLE")
+data class TransferEntity(
+    @PrimaryKey
+    val id:Int? = null,
+    val playerName:String? = null,
+    val date:String? = null,
+    val transferType: String? = null,
+    val clubNameIN: String? = null,
+    val clubLogoIN: String? = null,
+    val clubNameOut: String? = null,
+    val clubLogoOut: String? = null,
+)
+
