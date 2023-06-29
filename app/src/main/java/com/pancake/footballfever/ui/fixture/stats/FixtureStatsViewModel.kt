7f259e54d9c2f9b3c50825b3bedc9219cace72ca
kareem <skrym826@gmail.com>
2023-06-29 19:32:59 +0300

    update FixtureFragment,fixtureStatsFragment and ViewModel

--- app/src/main/java/com/pancake/footballfever/ui/fixture/stats/FixtureStatsViewModel.kt
+++ app/src/main/java/com/pancake/footballfever/ui/fixture/stats/FixtureStatsViewModel.kt
@@ -1,9 +1,40 @@
 package com.pancake.footballfever.ui.fixture.stats
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pancake.footballfever.domain.usecases.FixtureStasticsUseCases.FetchFixtureStatisticsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FixtureStatsViewModel @Inject constructor(): ViewModel() {
+class FixtureStatsViewModel @Inject constructor(
+    private val fetchFixtureStatisticsUseCase: FetchFixtureStatisticsUseCase,
+) :
+    ViewModel() {
+    private val _fixtureStatistics = MutableStateFlow(FixtureStatsUiState())
+    val fixtureStats = _fixtureStatistics.asStateFlow()
+
+
+    fun fetchFixtureStats(fixtureId: Int) {
+        viewModelScope.launch {
+            val response = fetchFixtureStatisticsUseCase(fixtureId)
+            if (response.isSuccess){
+                _fixtureStatistics.update {
+                    it.copy(
+                        isLoading = false,
+                        success = response.getOrNull()
+                    )
+
+                }
+            }
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/pancake/footballfever/ui/fixture/stats/FragmentFixtureStats.kt
+++ app/src/main/java/com/pancake/footballfever/ui/fixture/stats/FragmentFixtureStats.kt
@@ -2,11 +2,16 @@ package com.pancake.footballfever.ui.fixture.stats
 
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.pancake.footballfever.R
 import com.pancake.footballfever.databinding.FragmentStaticisFixtureBinding
 import com.pancake.footballfever.ui.base.BaseFragment
 import com.pancake.footballfever.domain.Constants
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+private const val FIXTUREID = "fixtureId"
 
 @AndroidEntryPoint
 class FragmentFixtureStats : BaseFragment<FragmentStaticisFixtureBinding, FixtureStatsViewModel>() {
@@ -14,14 +19,33 @@ class FragmentFixtureStats : BaseFragment<FragmentStaticisFixtureBinding, Fixtur
         get() = R.layout.fragment_staticis_fixture
     override val viewModel by viewModels<FixtureStatsViewModel>()
 
+    override fun setup() {
+        super.setup()
+        val fixtureId = requireArguments().getInt(FIXTUREID)
+        fixtureId.let {
+            viewModel.fetchFixtureStats(it)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.fixtureStats.collect { fixtureStats ->
+                val fixture = fixtureStats.success
+                if (fixture != null) {
+                    binding.item = fixture
+                }
+            }
+        }
+
+    }
+
 
     companion object {
         @JvmStatic
         fun newInstance(fixtureId: Int?) =
             FragmentFixtureStats().apply {
                 arguments = Bundle().apply {
-                    fixtureId?.let { putInt(Constants.FIXTURE_ID, it) }
+                    fixtureId?.let { putInt(FIXTUREID, it) }
                 }
             }
     }
+
 }
