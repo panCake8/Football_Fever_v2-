693f7d87747548583abfd68a4d0ae3f54b373ece
kareem <skrym826@gmail.com>
2023-06-29 19:31:31 +0300

    update StatisticsDto

--- app/src/main/java/com/pancake/footballfever/data/remote/dto/StatisticsDto.kt
+++ app/src/main/java/com/pancake/footballfever/data/remote/dto/StatisticsDto.kt
@@ -1,6 +1,7 @@
 package com.example.footboolfever.data.remote.dto
 
 import com.example.footboolfever.data.remote.dto.utils.Goals
+import com.example.footboolfever.data.remote.dto.utils.League
 import com.example.footboolfever.data.remote.dto.utils.Team
 import com.google.gson.annotations.SerializedName
 
@@ -9,6 +10,11 @@ data class StatisticsDto(
     @field:SerializedName("team")
     val team: Team? = null,
 
+    @SerializedName("goals")
+    val goals: Goals? = null,
+
+    @SerializedName("league")
+    val league: League? = null,
 
     @field:SerializedName("statistics")
     val statistics: List<StatisticsItem?>? = null
